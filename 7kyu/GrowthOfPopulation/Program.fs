3e77547aa87084c2e9a35e8cc33be3fdd83ee5de
nobodyshere <heybroken@gmail.com>
2020-05-23 21:32:32 +0300

    add some katas

---
+++ 7kyu/GrowthOfPopulation/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+
+// https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/fsharp
+
+// In a small town the population is p0 = 1000 at the beginning of a year.
+// The population regularly increases by 2 percent per year and moreover 50 new
+// inhabitants per year come to live in the town.
+// How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+open System
+
+[<EntryPoint>]
+let main argv =
+    let nbYear (p0: int) (percent: float) (aug: int) (p: int) =
+        let mutable (currentPopulation: int) = p0
+        let mutable (year: int) = 0
+        while currentPopulation < p do
+            currentPopulation <- currentPopulation + int (float currentPopulation * (percent / 100.)) + aug
+            year <- year + 1
+        year
+
+    let rec betterNbYear (p0: int) (percent: float) (aug: int) (p: int) =
+        match (p0, p) with
+        | (p0, p) when p0 >= p -> 0
+        | _ -> 1 + (betterNbYear (int (float (p0) + float (p0) / 100.0 * percent + float (aug))) percent aug p)
+
+    printfn "%i" (nbYear 1000 2.0 50 1200)
+    printfn "%i" (nbYear 1500 5.0 100 5000)
+    printfn "%i" (betterNbYear 1500000 2.5 10000 2000000)
+    printfn "%i" (betterNbYear 1500000 0.25 1000 2000000)
+    0 // return an integer exit code
---
+++ 7kyu/TwoToOne/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+
+// https://www.codewars.com/kata/5656b6906de340bd1b0000ac
+// Take 2 strings s1 and s2 including only letters from ato z.
+// Return a new sorted string, the longest possible, containing distinct letters
+
+open System
+open System.Linq
+
+[<EntryPoint>]
+let main argv =
+    let longest (s1: string) (s2: string) =
+        Array.append (s1.ToCharArray()) (s2.ToCharArray())
+        |> Array.sort
+        |> Array.distinct
+        |> System.String.Concat
+
+    let shorterLongest (s1: string) (s2: string) =
+        s1 + s2
+        |> Seq.distinct
+        |> Seq.sort
+        |> String.Concat
+
+    printfn "%s" (longest "hello" "world")
+    printfn "%s" (shorterLongest "hello" "world")
+    0 // return an integer exit code
