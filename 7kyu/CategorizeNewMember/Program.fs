255edab487e3fb2bff05e0ce95b86c48241d9e2f
nobodyshere <heybroken@gmail.com>
2020-05-25 22:30:33 +0300

    add two katas

---
+++ 7kyu/CategorizeNewMember/Program.fs
@@ -0,0 +1,47 @@
+﻿// Learn more about F# at http://fsharp.org
+
+// https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/fsharp
+
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open.
+// They would like your help with an application form that will tell prospective members which category they will be placed.
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7.
+// In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+// Input
+// Input will consist of a list of lists containing two items each. Each list contains information for a single potential member.
+// Information consists of an integer for the person's age and an integer for the person's handicap.
+// Note for F#: The input will be of (int list list) which is a List<List>
+
+open System
+open System.Collections.Generic
+
+[<EntryPoint>]
+let main argv =
+    let OpenOrSenior (xs: int list list) =
+        xs
+        |> List.map (fun x ->
+            if x.[0] > 54 && x.[1] > 7 then "Senior" else "Open")
+
+    let AnotherOpenOrSenior = List.map (function
+        | [age; handicap] when age > 54 && handicap > 7 -> "Senior"
+        | _ -> "Open" )
+
+
+    printfn "%A"
+        (OpenOrSenior
+            [ [ 10; 3 ]
+              [ 75; 8 ] ])
+
+    printfn "%A"
+        (OpenOrSenior
+            [ [ 20; 4 ]
+              [ 23; 6 ] ])
+
+    printfn "%A"
+        (AnotherOpenOrSenior
+            [ [ 18; 20 ]
+              [ 45; 2 ]
+              [ 61; 12 ]
+              [ 37; 6 ]
+              [ 21; 21 ]
+              [ 78; 9 ] ])
+    0 // return an integer exit code
---
+++ 7kyu/DontGiveMeFive/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+// https://www.codewars.com/kata/5813d19765d81c592200001a/train/fsharp
+// In this kata you get the start number and the end number of a region and should return
+// the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+open System
+
+[<EntryPoint>]
+let main argv =
+    let dontGiveMeFive startValue endValue =
+        match (startValue, endValue) with
+        | startValue, endValue when startValue < 5 && endValue > 5 -> Seq.length (seq {startValue .. endValue}) - 1
+        | _ -> Seq.length (seq {startValue .. endValue})
+
+
+    let AnotherDontGiveMeFive startValue endValue =
+        {startValue .. endValue}
+        |> Seq.sumBy (fun x -> if x.ToString().IndexOf('5') = -1 then 1 else 0)
+
+    printfn "%i" (dontGiveMeFive 1 9)
+    printfn "%i" (AnotherDontGiveMeFive 4 17)
+    printfn "%i" (AnotherDontGiveMeFive 6 11)
+    0 // return an integer exit code
