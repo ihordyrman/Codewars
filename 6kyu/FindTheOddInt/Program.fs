9c74e4137df4d6b3395257871d350d0b682fdc2c
nobodyshere <heybroken@gmail.com>
2020-05-30 21:01:41 +0300

    add 6kyu

---
+++ 6kyu/FindTheOddInt/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+
+// https://www.codewars.com/kata/54da5a58ea159efa38000836/train/fsharp
+// Given an array, find the integer that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let inline findOdd (numbers: List<int>) = 
+        numbers 
+        |> List.countBy id
+        |> List.find (fun (_, count) -> count % 2 = 1)
+        |> fst 
+    
+    let cleverFindOdd numbers = List.reduce (^^^)
+
+    let n = [20; 1; -1; 2; -2; 3; 3; 5; 5; 1; 2; 4; 20; 4; -1; -2; 5]
+    printfn "%A" (findOdd n)
+    printfn "%A" (cleverFindOdd n)
+    0 // return an integer exit code
---
+++ 6kyu/MultiplesOf3or5/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+// https://www.codewars.com/kata/514b92a657cdc65150000006/train/fsharp
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+// The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let solve n = 
+        seq { 1 .. n - 1} 
+        |> Seq.filter (fun x -> x % 3 = 0 || x % 5 = 0) 
+        |> Seq.sum
+
+    let anotherSolve n = 
+        seq { for x in 3..(n - 1) do if x % 3 = 0 || x % 5 = 0 then yield x } 
+        |> Seq.sum
+
+    printfn "%A" (solve 12)
+    printfn "%A" (anotherSolve 12)
+    0 // return an integer exit code
