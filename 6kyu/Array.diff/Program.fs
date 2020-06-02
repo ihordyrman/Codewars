2004bb8eaf88a44701bc0c2cb03c6057d5c60b33
nobodyshere <heybroken@gmail.com>
2020-06-02 20:51:13 +0300

    add Array.diff / CountDuplicates

---
+++ 6kyu/Array.diff/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+
+// https://www.codewars.com/kata/523f5d21c841566fde000009/train/fsharp
+
+// Your goal in this kata is to implement a difference function,
+// which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b.
+open System
+
+[<EntryPoint>]
+let main argv =
+    let arrayDiff (itemsToExclude: int[]) (source:int[]) =
+        source |> Array.filter (fun x -> not (Array.contains x itemsToExclude))
+
+    let anotherArrayDiff itemsToExclude source =
+        let itemsToExclude = itemsToExclude |> Set.ofArray
+        source
+        |> Array.choose (fun x -> if itemsToExclude.Contains x then None else Some x)
+
+    printfn "%A" (arrayDiff [|1; 1; 1|] [|1; 2|])
+    printfn "%A" (anotherArrayDiff [|1; 1; 1|] [|1; 2|])
+    0 // return an integer exit code
---
+++ 6kyu/CountingDuplicates/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+// https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/fsharp
+
+// Write a function that will return the count of distinct case-insensitive alphabetic characters
+// and numeric digits that occur more than once in the input string.
+// The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+open System.Linq
+
+[<EntryPoint>]
+let main argv =
+    let duplicateCount (text : string) =
+        text.ToLower().GroupBy(fun x -> x).Count(fun y -> y.Count() > 1);
+
+    let betterDuplicateCount (text: string) =
+        text.ToLower()
+        |> Seq.countBy id
+        |> Seq.filter (snd >> ((<)1))
+        |> Seq.length
+
+    0 // return an integer exit code
