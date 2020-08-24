2ea99cc5e603761b01f182ad6069ccd71f67e905
nobodyshere <heybroken@gmail.com>
2020-08-24 17:42:47 +0300

    add Backspaces in string

---
+++ c#/6kyu/Backspaces in string/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Backspaces_in_string
+{
+    /*
+     * Backspaces in string
+     * https://www.codewars.com/kata/5727bb0fe81185ae62000ae3/train/csharp
+     *
+     * Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+     * Your task is to process a string with "#" symbols.
+     */
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(CleanString("abc#d##c"));
+            Console.WriteLine(CleanString("abc##d######"));
+            Console.WriteLine(CleanString("abc####d##c#"));
+            Console.WriteLine(CleanString("#######"));
+            Console.WriteLine(CleanString(""));
+        }
+
+        private static string CleanString(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return string.Empty;
+
+            var result = string.Empty;
+
+            foreach (var ch in s)
+            {
+                if (ch != '#')
+                {
+                    result += ch;
+                    continue;
+                }
+
+                if (result.Length <= 0) continue;
+                result = result.Remove(result.Length - 1, 1);
+            }
+
+            return result;
+        }
+    }
+}
