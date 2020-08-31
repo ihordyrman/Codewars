665cbd00d2ba19114d6f0f8d66b0acb4b31b69cd
nobodyshere <heybroken@gmail.com>
2020-08-31 19:03:28 +0300

    add Mexican Wave

---
+++ c#/6kyu/Mexican Wave/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mexican_Wave
+{
+    /*
+     * https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/train/csharp
+     * Mexican Wave
+     *
+     * In this simple Kata your task is to create a function that turns a string into a Mexican Wave.
+     * You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+    */
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Wave("hello"));
+        }
+
+        private static List<string> Wave(string str) =>
+            str.Select((c, i) => str.Substring(0, i) + char.ToUpper(c) + str.Substring(i + 1))
+                .Where(x => x != str)
+                .ToList();
+    }
+}
