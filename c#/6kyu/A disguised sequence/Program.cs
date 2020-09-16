d5d0d946752397b15e4aaf293d365d57dec9ce7e
nobodyshere <heybroken@gmail.com>
2020-09-16 18:22:09 +0300

    add A disguised sequence

---
+++ c#/6kyu/A disguised sequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace A_disguised_sequence
+{
+    // A disguised sequence (I)
+    // Given u0 = 1, u1 = 2 and the relation 6unun+1-5unun+2+un+1un+2 = 0 calculate un for any integer n >= 0.
+    // fcn(n) returns un: fcn(17) -> 131072, fcn(21) -> 2097152
+    // You can take two points of view to do this kata:
+    // the first one purely algorithmic from the definition of un
+    // the second one - not at all mandatory, but as a complement - is to get a bit your head around and find which sequence is hidden behind un.
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(Fcn(17));
+            Console.WriteLine(Fcn(21));
+            Console.WriteLine(Fcn(14));
+            Console.WriteLine(Fcn(43));
+            Console.WriteLine(Fcn(19));
+        }
+
+        private static BigInteger Fcn(int n)
+        {
+            return new BigInteger(Math.Pow(2.0, n));
+        }
+    }
+}
