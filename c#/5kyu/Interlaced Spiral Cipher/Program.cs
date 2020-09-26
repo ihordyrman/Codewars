f22e9d1f735f72c462fc0471f8fcc2032b2876e1
nobodyshere <heybroken@gmail.com>
2020-09-26 22:44:19 +0300

    string doubles

---
+++ c#/5kyu/Interlaced Spiral Cipher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Interlaced_Spiral_Cipher
+{
+    // In this kata, your task is to implement what I call Interlaced Spiral Cipher (ISC).
+    // Encoding a string using ISC is achieved with the following steps:
+    // 1. Form a square large enough to fit all the string characters
+    // 2. Starting with the top-left corner, place string characters in the corner cells moving in a clockwise direction
+    // 3. After the first cycle is complete, continue placing characters in the cells following the last one in its respective row/column
+    // 4. When the outer cells are filled, repeat steps 2 through 4 for the remaining inner squares (refer to the example below for further clarification)
+    // 5. Fill up any unused cells with a space character and return the rows joined together.
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Encode("Hello World!"));
+        }
+
+        public static string Encode(string s)
+        {
+            // Form a square large enough to fit all the string characters
+
+            // Starting with the top-left corner, place string characters in the corner cells moving in a clockwise direction
+
+            // After the first cycle is complete, continue placing characters in the cells following the last one in its respective row/column
+
+            // When the outer cells are filled, repeat steps 2 through 4 for the remaining inner squares (refer to the example below for further clarification)
+            
+
+            // Fill up any unused cells with a space character and return the rows joined together.
+            return string.Empty;
+        }
+        public static string Decode(string s)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ c#/7kyu/String Doubles/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace String_Doubles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DoublesRegex("abbbzz"));
+            Console.WriteLine(DoublesRegex("zzzzykkkd"));
+            Console.WriteLine(DoublesRegex("abbcccdddda"));
+            Console.WriteLine(DoublesRegex("vvvvvoiiiiin"));
+            Console.WriteLine(DoublesRegex("rrrmooomqqqqj"));
+            Console.WriteLine(DoublesRegex("xxbnnnnnyaaaaam"));
+        }
+
+        private static string Doubles(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            var result = string.Empty;
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i + 1] == str[i])
+                {
+                    i++;
+                    continue;
+                }
+
+                result += str[i];
+            }
+
+            return (str[^1] != str[^2] || (str[^2] == str[^1] && str[^3] == str[^1])) ? result + str[^1] : result;
+        }
+
+        private static string DoublesRegex(string str) {
+            return Regex.Replace(str, @"(\w)\1", "");
+        }
+    }
+}
