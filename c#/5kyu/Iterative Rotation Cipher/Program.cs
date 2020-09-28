0d134da31b870217a6c7dc6558c2f4d2e63a3776
nobodyshere <heybroken@gmail.com>
2020-09-28 22:09:25 +0300

    Almost done. Need to fix

--- c#/5kyu/Iterative Rotation Cipher/Program.cs
+++ c#/5kyu/Iterative Rotation Cipher/Program.cs
@@ -7,7 +7,10 @@ namespace Iterative_Rotation_Cipher
     {
         private static void Main()
         {
-            Console.WriteLine(Encode(10, "If you wish to make an apple pie from scratch, you must first invent the universe."));
+            var encoded = Encode(10, "If you wish to make an apple pie from scratch, you must first invent the universe.");
+            Console.WriteLine(encoded);
+            var decoded = Decode(encoded);
+            Console.WriteLine(decoded);
         }
 
         public static string Encode(int n, string s)
@@ -31,12 +34,35 @@ namespace Iterative_Rotation_Cipher
                     arr[i] = arr[i][^1] + arr[i][..^1];
             }
 
-            return string.Join(" ", arr);
+            return n + " " + string.Join(" ", arr);
         }
         public static string Decode(string s)
         {
-            // todo: finish
-            return null;
+            var arr = s.Split(" ");
+            var n = int.Parse(arr[0]);
+            var newArr = new string[arr.Length - 1];
+            Array.Copy(arr, 1, newArr, 0, newArr.Length);
+
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    newArr[i] = newArr[i][^1] + newArr[i][..^1];
+            }
+
+            s = string.Join(" ", newArr);
+
+            var spaces = new List<int>();
+
+            for (int i = 0; i < s.Length; i++)
+                if (s[i] == ' ') spaces.Add(i);
+
+            s = s.Replace(" ", "");
+            s =  s.Substring(n) + s.Substring(0, n);
+
+            for (int i = 0; i < spaces.Count; i++)
+                s = s.Insert(spaces[i], " ");
+
+            return s;
         }
     }
 }
