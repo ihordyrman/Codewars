ba169816c13cb93a6c665ea1591edc1d4c23269e
nobodyshere <heybroken@gmail.com>
2020-10-02 17:54:27 +0300

    unfinished kata

---
+++ c#/4kyu/Befunge Interpreter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Befunge_Interpreter
+{
+    internal static class Program
+    {
+        // https://www.codewars.com/kata/526c7b931666d07889000a3c/train/csharp
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static string Interpret(string code)
+        {
+            // todo: finish kata
+            return "";
+        }
+    }
+}
