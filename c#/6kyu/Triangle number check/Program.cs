af8666be0b06b2700aa6e62a99a42e17bb481e5e
nobodyshere <heybroken@gmail.com>
2020-10-03 19:02:10 +0300

    add catas

---
+++ c#/6kyu/Triangle number check/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Triangle_number_check
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(BestIsTriangleNumber(10));
+            Console.WriteLine(IsTriangleNumber(8));
+        }
+
+        private static bool IsTriangleNumber(long number)
+        {
+            long sum = 0;
+            for (long i = 1; sum <= number; i++)
+            {
+                sum += i;
+                if (sum == number)
+                    return true;
+            }
+            return false;
+        }
+
+        private static bool BestIsTriangleNumber(long number) => Math.Sqrt(1 + 8 * number) % 1 == 0;
+    }
+}
---
+++ c#/7kyu/Say Me Please Operations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Say_Me_Please_Operations
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(SayMeOperations("1 2 3 5 8"));
+            Console.WriteLine(SayMeOperations("9 4 5 20 25"));
+            Console.WriteLine(SayMeOperations("10 2 5 -3 -15 12"));
+            Console.WriteLine(SayMeOperations("2 2 4"));
+        }
+
+        private static string SayMeOperations(string stringNumbers)
+        {
+            var arr = stringNumbers.Split(" ").Select(int.Parse).ToArray();
+            var result = new string[arr.Length - 2];
+
+            for (var i = 1; i < arr.Length - 1; i++)
+                result[i - 1] = arr[i - 1] + arr[i] == arr[i + 1] ? "addition" :
+                    arr[i - 1] * arr[i] == arr[i + 1] ? "multiplication" :
+                    arr[i - 1] - arr[i] == arr[i + 1] ? "subtraction" :
+                    arr[i - 1] / arr[i] == arr[i + 1] ? "division" : throw new Exception("fuck off");
+
+            return string.Join(", ", result);
+        }
+    }
+}
