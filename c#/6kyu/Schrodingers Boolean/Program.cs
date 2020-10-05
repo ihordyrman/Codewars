195eb6a4e747e50236d814cefde540f7f5fddbb6
nobodyshere <heybroken@gmail.com>
2020-10-05 21:12:10 +0300

    add Schrodingers Boolean

---
+++ c#/6kyu/Schrodingers Boolean/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Schrodingers_Boolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Kata
+    {
+        private static bool x = false;
+        public static bool omnibool => x != !x;
+    }
+}
