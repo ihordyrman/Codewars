7dd91fe06adead82293b0c4e13e5d5fa082d177b
nobodyshere <ihor.dyrman@outlook.com>
2020-11-08 21:05:24 +0200

    add 1n-Cycle

---
+++ c#/6kyu/1n-Cycle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1n_Cycle
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(Running(7));
+            Console.WriteLine(Running(5));
+            Console.WriteLine(Running(13));
+            Console.WriteLine(Running(94));
+            Console.WriteLine(Running(37));
+            Console.WriteLine(Running(33));
+            Console.WriteLine(Running(27));
+        }
+
+        private static int Running(int n)
+        {
+            if (n % 2 == 0 || n % 5 == 0)
+                return -1;
+
+            var result = 1;
+
+            for (var i = 1; i <= n; i++)
+            {
+                result = result * 10 % n;
+                if (result == 1) 
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
